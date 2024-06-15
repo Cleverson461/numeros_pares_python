cbb233212f7e98d10819e7d58e05bc4a39f36b56
Cleverson461 <cleverson461@gmail.com>
2024-06-15 10:44:55 -0400

    first commit

---
+++ 19_exercicio_numeros_pares.py
@@ -0,0 +1,9 @@
+# Receba um numero e mostre todos os numeros pares de 0 até o numero digitado.
+
+n1 = int(input('Digite um numero: '))
+
+i = 1
+while i <= n1:
+  if i % 2 == 0:
+    print(i)
+  i += 1
